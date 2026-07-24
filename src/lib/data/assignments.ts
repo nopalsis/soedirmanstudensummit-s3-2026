53f1ec375c2c1a6cf48c02784891f7654437b471
nopalsis <naufal.sis16@gmail.com>
2026-07-24 14:12:39 +0700

    fix : content assignment, and countdown

--- src/lib/data/assignments.ts
+++ src/lib/data/assignments.ts
@@ -29,10 +29,10 @@ export const assignments: Assignment[] = [
       longDescription:
         "Membuat video berupa perkenalan diri yang bertujuan mendorong mahasiswa baru lebih percaya diri dalam menunjukkan potensi dan karakter diri.",
       openedAt: new Date(Date.now() + 0 * 24 * 60 * 60 * 1000),
-      deadlines: {
-        "SNBP-SNBT": new Date(Date.now() + -3 * 24 * 60 * 60 * 1000),
-        "Mandiri": createDeadlineDate(16),
-      },
+        deadlines: {
+         "SNBP-SNBT": new Date(Date.now() + -3 * 24 * 60 * 60 * 1000),
+         "Mandiri": createDeadlineDate(15),
+       },
     },
     {
       name: "Words of Me",
@@ -41,9 +41,9 @@ export const assignments: Assignment[] = [
         "Words of Me bertujuan memotivasi mahasiswa baru untuk menetapkan tujuan hidup serta arah pengembangan diri selama menempuh pendidikan di perguruan tinggi.",
       openedAt: new Date(Date.now() + 0 * 24 * 60 * 60 * 1000),
        deadlines: {
-        "SNBP-SNBT": new Date(Date.now() + -3 * 24 * 60 * 60 * 1000),
-        "Mandiri": createDeadlineDate(16),
-      },
+         "SNBP-SNBT": new Date(Date.now() + -3 * 24 * 60 * 60 * 1000),
+         "Mandiri": createDeadlineDate(15),
+       },
     },
     {
       name: "Bintang Soedirman Muda",
@@ -58,79 +58,79 @@ export const assignments: Assignment[] = [
       description: "Ikuti talkshow pengenalan UKM dan fakultas.",
       longDescription:
         "Talkshow branding bertujuan memperkenalkan fakultas dan UKM di Universitas Jenderal Soedirman agar mahasiswa baru mengenal lingkungan kampus dan memilih UKM sesuai minat serta bakat.",
-      openedAt: new Date(Date.now() + 0 * 24 * 60 * 60 * 1000),
-      deadline: createDeadlineDate(0),
-    },
-    {
-      name: "LinkedIn",
+       openedAt: new Date(Date.now() + 0 * 24 * 60 * 60 * 1000),
+       deadline: createDeadlineDate(-1),
+     },
+     {
+       name: "LinkedIn",
       description: "Lengkapi profil LinkedIn.",
       longDescription:
         "Mahasiswa baru diharapkan memahami pemanfaatan LinkedIn sebagai sarana pengembangan diri, memperluas jejaring profesional, melatih komunikasi formal, serta mendokumentasikan pengalaman secara terstruktur.",
-      openedAt: new Date(Date.now() + 0 * 24 * 60 * 60 * 1000),
-      deadline: createDeadlineDate(5),
-    },
-    {
-      name: "Feel The Vibes",
+       openedAt: new Date(Date.now() + 0 * 24 * 60 * 60 * 1000),
+       deadline: createDeadlineDate(4),
+     },
+     {
+       name: "Feel The Vibes",
       description: "Ikuti kegiatan flashmob.",
       longDescription:
         "Flashmob menjadi media ekspresi kolektif yang membangun kebersamaan, kekompakan, dan semangat mahasiswa baru sebagai Soedirman Muda.",
-      openedAt: new Date(Date.now() + 0 * 24 * 60 * 60 * 1000),
-      deadline: createDeadlineDate(5),
-    },
-    {
-      name: "Luminous Boundaries",
+       openedAt: new Date(Date.now() + 0 * 24 * 60 * 59 * 1000),
+       deadline: createDeadlineDate(4),
+     },
+     {
+       name: "Luminous Boundaries",
       description: "Pahami pentingnya batasan diri.",
       longDescription:
         "Bertujuan meningkatkan kesadaran mahasiswa baru mengenai pentingnya memahami dan menghormati batasan diri maupun orang lain sebagai upaya pencegahan kekerasan dan kekerasan seksual.",
-      openedAt: new Date(Date.now() + 0 * 24 * 60 * 60 * 1000),
-      deadline: createDeadlineDate(10),
-    },
-    {
-      name: "Mind Sync Session",
+       openedAt: new Date(Date.now() + 0 * 24 * 60 * 60 * 1000),
+       deadline: createDeadlineDate(9),
+     },
+     {
+       name: "Mind Sync Session",
       description: "Ikuti podcast pengembangan diri.",
       longDescription:
         "Podcast yang membantu mahasiswa baru mengembangkan growth mindset, mengenali potensi diri, serta meningkatkan kemampuan beradaptasi secara akademik, emosional, dan sosial.",
-      openedAt: new Date(Date.now() + 0 * 24 * 60 * 60 * 1000),
-      deadline: createDeadlineDate(14),
-    },
-    {
-      name: "Twibbon",
+       openedAt: new Date(Date.now() + 0 * 24 * 60 * 60 * 1000),
+       deadline: createDeadlineDate(13),
+     },
+     {
+       name: "Twibbon",
       description: "Unggah Twibbon resmi.",
       longDescription:
         "Twibbon menjadi media perkenalan yang membantu mahasiswa baru mengekspresikan identitas diri, membangun rasa bangga dan solidaritas, serta memanfaatkan media sosial secara bijak.",
-      openedAt: createDeadlineDate(16),
-      deadline: createDeadlineDate(16),
-    },
-    {
-      name: "From A Beginning to A New Chapter (Life Mapping | Add Yours)",
+       openedAt: createDeadlineDate(16),
+       deadline: createDeadlineDate(15),
+     },
+     {
+       name: "From A Beginning to A New Chapter (Life Mapping | Add Yours)",
       description: "Buat Life Mapping.",
       longDescription:
         "Mahasiswa baru diajak merefleksikan perjalanan hidup, memahami proses yang membentuk diri, serta menggambarkan harapan dan tujuan yang ingin dicapai selama menjalani kehidupan kampus.",
-      openedAt: new Date(Date.now() + 17 * 24 * 60 * 60 * 1000),
-      deadline: createDeadlineDate(16),
-    },
-    {
-      name: "SOLO (School of Leadership Organization)",
+       openedAt: new Date(Date.now() + 17 * 24 * 60 * 60 * 1000),
+       deadline: createDeadlineDate(15),
+     },
+     {
+       name: "SOLO (School of Leadership Organization)",
       description: "Ikuti program pengembangan kepemimpinan.",
       longDescription:
         "Program pengembangan kepemimpinan yang mendorong mahasiswa baru mengenali potensi diri, merefleksikan perjalanan kepemimpinan, serta membangun jiwa kepemimpinan sebagai bekal menjadi pemimpin yang berdampak.",
-      openedAt: new Date(Date.now() + 17 * 24 * 60 * 60 * 1000),
-      deadline: createDeadlineDate(16),
-    },
-    {
-      name: "Yel-Yel Kelompok",
+       openedAt: new Date(Date.now() + 17 * 24 * 60 * 60 * 1000),
+       deadline: createDeadlineDate(15),
+     },
+     {
+       name: "Yel-Yel Kelompok",
       description: "Buat yel-yel kelompok.",
       longDescription:
         "Membuat yel-yel kelompok sebagai media memperkenalkan identitas kelompok sekaligus membangun kekompakan antaranggota.",
-      openedAt: new Date(Date.now() + 17 * 24 * 60 * 60 * 1000),
-      deadline: createDeadlineDate(18),
-    },
-    {
-      name: "Papan Kelompok",
+       openedAt: new Date(Date.now() + 17 * 24 * 60 * 60 * 1000),
+       deadline: createDeadlineDate(17),
+     },
+     {
+       name: "Papan Kelompok",
       description: "Buat papan informasi kelompok.",
       longDescription:
         "Membuat papan informasi secara kreatif yang merepresentasikan identitas kelompok mahasiswa baru.",
-      openedAt: new Date(Date.now() + 19 * 24 * 60 * 60 * 1000),
-      deadline: createDeadlineDate(18),
-    },
+       openedAt: new Date(Date.now() + 19 * 24 * 60 * 60 * 1000),
+       deadline: createDeadlineDate(17),
+     },
 ];
