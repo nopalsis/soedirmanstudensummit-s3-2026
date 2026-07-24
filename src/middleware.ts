607e1aaba051d30a68a0865c178b56bc02084873
nopalsis <naufal.sis16@gmail.com>
2026-07-24 13:13:13 +0700

    feat : handle 404 page

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,5 @@
 import { defineMiddleware } from 'astro:middleware';
 
-const ALLOWED_ROUTES = ['/', '/assignment'];
-
 export const onRequest = defineMiddleware(async (context, next) => {
-  const { pathname } = context.url;
-
-  const isAllowed = ALLOWED_ROUTES.some(route => 
-    pathname === route || pathname.startsWith(route + '/')
-  );
-
-  if (!isAllowed && pathname !== '/maintenance' && !pathname.startsWith('/_')) {
-    return context.redirect('/maintenance');
-  }
-
   return next();
 });
